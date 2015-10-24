a0aa8ed787c3ea32c94c5c8e9471953d17cfd13b
Brunno dos Santos <squiter85@gmail.com>
2015-10-24 16:58:57 -0200

    Getting longitude, latitude and speed

--- core location/Controllers/ViewController.m
+++ core location/Controllers/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+
+@property (nonatomic, weak) IBOutlet UILabel *longt;
+@property (nonatomic, weak) IBOutlet UILabel *latt;
+@property (nonatomic, weak) IBOutlet UILabel *speed;
+
+@property (nonatomic, strong) CLLocationManager *locationManager;
 
 @end
 
@@ -17,11 +24,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    [self.locationManager requestWhenInUseAuthorization];
+    // ou
+    //[self.locationManager requestAlwaysAuthorization];
+    
+    [self.locationManager startUpdatingLocation];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - CLLocationManagerDelegate
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *location = [locations lastObject];
+    
+    self.latt.text = [NSString stringWithFormat:@"%.8f", location.coordinate.latitude];
+    self.longt.text = [NSString stringWithFormat:@"%.8f", location.coordinate.longitude];
+    self.speed.text = [NSString stringWithFormat:@"%.8f", location.speed];
 }
 
 @end
